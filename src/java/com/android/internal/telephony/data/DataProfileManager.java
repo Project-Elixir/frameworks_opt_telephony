172858864a75028f835f87394b93dade8fa1d44c
Nishant Kumar <www.rajsonu13@gmail.com>
2023-12-24 13:27:30 +0530

    telephony: Make a few members accessible
    * Required for qti telephony

--- src/java/com/android/internal/telephony/data/DataProfileManager.java
+++ src/java/com/android/internal/telephony/data/DataProfileManager.java
@@ -80,10 +80,10 @@ public class DataProfileManager extends Handler {
     private final LocalLog mLocalLog = new LocalLog(128);
 
     /** Data network controller. */
-    private final @NonNull DataNetworkController mDataNetworkController;
+    protected final @NonNull DataNetworkController mDataNetworkController;
 
     /** Data config manager. */
-    private final @NonNull DataConfigManager mDataConfigManager;
+    protected final @NonNull DataConfigManager mDataConfigManager;
 
     /** Cellular data service. */
     private final @NonNull DataServiceManager mWwanDataServiceManager;
@@ -1163,7 +1163,7 @@ public class DataProfileManager extends Handler {
      * Log debug messages.
      * @param s debug messages
      */
-    private void log(@NonNull String s) {
+    protected void log(@NonNull String s) {
         Rlog.d(mLogTag, s);
     }
 
@@ -1171,7 +1171,7 @@ public class DataProfileManager extends Handler {
      * Log error messages.
      * @param s error messages
      */
-    private void loge(@NonNull String s) {
+    protected void loge(@NonNull String s) {
         Rlog.e(mLogTag, s);
     }
 
@@ -1187,7 +1187,7 @@ public class DataProfileManager extends Handler {
      * Log debug messages and also log into the local log.
      * @param s debug messages
      */
-    private void logl(@NonNull String s) {
+    protected void logl(@NonNull String s) {
         log(s);
         mLocalLog.log(s);
     }
